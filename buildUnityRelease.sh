d30811743633c9d5a3d3a0d7b88f238540f59f37
Brandon Catcho <lochnessy@gmail.com>
2016-08-10 00:02:21 -0700

    + unity release script

---
+++ buildUnityRelease.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set +e
+
+if [ -z "$1" ]; then
+  echo "Error. Please include a version number."
+  echo "Usage:"
+  echo "  ./buildUnityRelease.sh v0.1.0"
+  exit 1
+else
+  echo "==> Building unity release for version $1"
+fi
+
+if [ ! -d "tmp" ]; then
+  mkdir tmp
+fi
+cd tmp
+
+dirname="transition_$1"
+if [ -d "$dirname" ]; then
+  rm -rf $dirname
+fi
+mkdir $dirname
+
+cp ../Transition/*.cs $dirname
+cp -R ../Transition/Compiler $dirname
+cp -R ../Transition/Actions $dirname
+zip -r "transition_$1.zip" $dirname
+
+echo "==> Output: ${pwd}/transition_$1.zip"
+rm -rf $dirname
